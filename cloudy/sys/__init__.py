2daf46f70bfa1bc090503a653fe78b255a065dbb
Val Neekman <val@neekware.com>
2012-10-26 10:50:05 -0400

    auto load available approved commands

--- cloudy/sys/__init__.py
+++ cloudy/sys/__init__.py
@@ -1,3 +1,35 @@
-from core import *
-from etc import *
+import os
+import re
+import types
 
+PACKAGE = 'cloudy.sys'
+MODULE_RE = r"^.*.py$"
+PREFIX = ['sys_']
+
+# Search through every file inside this module.
+module_names = []
+module_dir = os.path.dirname( __file__)
+for filename in os.listdir(module_dir):
+    if not re.match(MODULE_RE, filename) or filename == "__init__.py":
+        continue
+    # Import the module file and find all function inside it.
+    module_module = __import__('%s.%s' % (PACKAGE, filename[:-3]),
+                              {}, {},
+                              filename[:-3])
+    for name in dir(module_module):
+        try:
+            prefix = name.split('_')[0]+'_'
+        except:
+            continue
+
+        if prefix in PREFIX:
+            item = getattr(module_module, name)
+            if not isinstance(item, (type, types.FunctionType)):
+                continue
+
+            # Found one, bring into the module namespace.
+            exec "%s = item" % name
+            module_names.append(name)
+
+# Hide everything other than the classes from other modules.
+__all__ = module_names
