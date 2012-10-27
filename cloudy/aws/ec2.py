6f078cf9dd40cfb1d31c3cbedc4acf477374c49f
Val L33 <val@neekware.com>
2012-10-27 19:23:55 -0400

    initial work on aws starts

--- cloudy/aws/ec2.py
+++ cloudy/aws/ec2.py
@@ -22,12 +22,17 @@ from cloudy.util.conf import CloudyConfig
 def aws_list_instances(cfg="~/.cloudy"):
     c = CloudyConfig(filenames=cfg)
     
-    # AWS_ACCESS_ID = c.cfg_grid['AWS']['AWS_ACCESS_ID']
-    # AWS_SECRET_KEY = c.cfg_grid['AWS']['AWS_SECRET_KEY']
-    # 
-    # Driver = get_driver(Provider.EC2)
-    # conn = Driver(AWS_ACCESS_ID, AWS_SECRET_KEY)
-    # nodes = conn.list_nodes()
-    # 
-    print c.cfg_grid
+    try:
+        AWS_ACCESS_ID = c.cfg_grid['AWS']['AWS_ACCESS_ID']
+        AWS_SECRET_KEY = c.cfg_grid['AWS']['AWS_SECRET_KEY']
+    except:
+        return
+    Driver = get_driver(Provider.EC2)
+    conn = Driver(AWS_ACCESS_ID, AWS_SECRET_KEY)
+    nodes = conn.list_nodes()
+    print nodes
+
+
+
+
     
