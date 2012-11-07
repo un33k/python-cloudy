7059ea90f0762c18598a7204ecf09b3d1cd5b720
Val Neekman <val@neekware.com>
2012-11-07 17:48:55 -0500

    add web server setup

--- cloudy/fabfile.py
+++ cloudy/fabfile.py
@@ -10,25 +10,31 @@ logging.getLogger().setLevel(logging.WARNING)
 
 def setup_db_postgres_primary(cfg_files='~/.cloudy'):
     cfg = CloudyConfig(filenames=cfg_files)
-    
     sys_update()
-    sys_safe_upgrade()
     sys_install_common()
-    sys_install_postfix()
-    sys_security_install_common()
-
+    
     # git info
     git_user_full_name = cfg.get_variable('common', 'git-user-full-name')
     git_user_email = cfg.get_variable('common', 'git-user-email')
     if git_user_full_name and git_user_email:
         sys_git_configure('root', git_user_full_name, git_user_email)
 
-
+    sys_safe_upgrade()
+    sys_install_postfix()
+    sys_security_install_common()
+    sys_set_default_editor()
+    
     # timezone and locale
     timezone = cfg.get_variable('common', 'timezone', 'Canada/Eastern')
     locale = cfg.get_variable('common', 'locale', 'en_US.UTF-8')
     sys_configure_timezone(timezone)
     sys_locale_configure()
+
+
+    # swap
+    swap = cfg.get_variable('ps_master', 'swap-size')
+    if swap:
+        sys_swap_configure(swap)
 
     # hostname, ips
     hostname = cfg.get_variable('pg_master', 'hostname')
@@ -75,6 +81,60 @@ def setup_db_postgres_primary(cfg_files='~/.cloudy'):
         db_pgis_get_database_gis_info('template_postgis')
 
 
+def setup_webserver(cfg_files='~/.cloudy'):
+    cfg = CloudyConfig(filenames=cfg_files)
+    sys_update()
+    sys_install_common()
+
+    # git info
+    git_user_full_name = cfg.get_variable('common', 'git-user-full-name')
+    git_user_email = cfg.get_variable('common', 'git-user-email')
+    if git_user_full_name and git_user_email:
+        sys_git_configure('root', git_user_full_name, git_user_email)
+
+
+    sys_safe_upgrade()
+    sys_install_postfix()
+    sys_security_install_common()
+    sys_memcached_install()
+    sys_memcached_configure_interface()
+    sys_python_install_common()
+    sys_set_default_editor()
+
+    # timezone and locale
+    timezone = cfg.get_variable('common', 'timezone', 'Canada/Eastern')
+    locale = cfg.get_variable('common', 'locale', 'en_US.UTF-8')
+    sys_configure_timezone(timezone)
+    sys_locale_configure()
+
+    # swap
+    swap = cfg.get_variable('webserver', 'swap-size')
+    if swap:
+        sys_swap_configure(swap)
+
+    # hostname, ips
+    hostname = cfg.get_variable('webserver', 'hostname')
+    if hostname:
+        sys_hostname_configure(hostname)
+        sys_add_hosts(hostname, '127.0.0.1')
+
+
+    # users & passwords
+    admin_user = cfg.get_variable('webserver', 'admin-user')
+    admin_pass = cfg.get_variable('webserver', 'admin-pass')
+    admin_group = cfg.get_variable('webserver', 'admin-group', 'admin')
+    if admin_user and admin_pass:
+        sys_user_add(admin_user)
+        sys_user_change_password(admin_user, admin_pass)
+        sys_user_add_sudoer(admin_user)
+        sys_user_set_group_umask(admin_user)
+        sys_user_create_group(admin_group)
+        sys_user_add_to_group(admin_user, admin_group)
+
+
+
+        
+        
 
 
 
