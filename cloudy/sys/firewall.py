a65d01c6fe9e3219bc4eb67ebeac15a7cef7187d
Val Neekman <val@neekware.com>
2012-11-14 17:12:43 -0500

    fix typo

--- cloudy/sys/firewall.py
+++ cloudy/sys/firewall.py
@@ -40,49 +40,49 @@ def sys_firewall_disable():
     sudo('ufw disable; sudo ufw status verbose')
 
 
-def sys_firwall_allow_incoming_http():
+def sys_firewall_allow_incoming_http():
     """ Allow http (port 80) requests to this server - Ex: (cmd)"""
     sudo('ufw allow http')
     sudo('ufw disable; echo "y" | ufw enable; sudo ufw status verbose')
 
 
-def sys_firwall_disallow_incoming_http():
+def sys_firewall_disallow_incoming_http():
     """ Disallow http (port 80) requests to this server - Ex: (cmd)"""
     sudo('ufw delete allow http')
     sudo('ufw disable; echo "y" | ufw enable; sudo ufw status verbose')
 
 
-def sys_firwall_allow_incoming_https():
+def sys_firewall_allow_incoming_https():
     """ Allow http (port 443) requests to this server - Ex: (cmd)"""
     sudo('ufw allow https')
     sudo('ufw disable; echo "y" | ufw enable; sudo ufw status verbose')
 
 
-def sys_firwall_disallow_incoming_https():
+def sys_firewall_disallow_incoming_https():
     """ Disallow http (port 443) requests to this server - Ex: (cmd)"""
     sudo('ufw delete allow https')
     sudo('ufw disable; echo "y" | ufw enable; sudo ufw status verbose')
 
 
-def sys_firwall_allow_incoming_postgresql():
+def sys_firewall_allow_incoming_postgresql():
     """ Allow postgresql (port 5432) requests to this server - Ex: (cmd)"""
     sudo('ufw allow postgresql')
     sudo('ufw disable; echo "y" | ufw enable; sudo ufw status verbose')
 
 
-def sys_firwall_disallow_incoming_postgresql():
+def sys_firewall_disallow_incoming_postgresql():
     """ Disallow postgresql (port 5432) requests to this server - Ex: (cmd)"""
     sudo('ufw delete allow postgresql')
     sudo('ufw disable; echo "y" | ufw enable; sudo ufw status verbose')
 
 
-def sys_firwall_allow_incoming_port(port):
+def sys_firewall_allow_incoming_port(port):
     """ Allow requests on specific port to this server - Ex: (cmd:<port>)"""
     sudo('ufw allow {0}}'.format(port))
     sudo('ufw disable; echo "y" | ufw enable; sudo ufw status verbose')
 
 
-def sys_firwall_disallow_incoming_port(port):
+def sys_firewall_disallow_incoming_port(port):
     """ Disallow requests to this server on specific port - Ex: (cmd:<port>)"""
     sudo('ufw delete allow {0}'.format(port))
     with settings(warn_only=False):
@@ -91,13 +91,13 @@ def sys_firwall_disallow_incoming_port(port):
     sudo('ufw disable; echo "y" | ufw enable; sudo ufw status verbose')
 
 
-def sys_firwall_allow_incoming_port_proto(port, proto):
+def sys_firewall_allow_incoming_port_proto(port, proto):
     """ Allow requests on specific port to this server - Ex: (cmd:<port>,<proto>)"""
     sudo('ufw allow {0}/{1}'.format(port, proto))
     sudo('ufw disable; echo "y" | ufw enable; sudo ufw status verbose')
 
 
-def sys_firwall_disallow_incoming_port_proto(port, proto):
+def sys_firewall_disallow_incoming_port_proto(port, proto):
     """ Disallow requests to this server on specific port - Ex: (cmd:<port>,<proto>)"""
     sudo('ufw delete allow {0}/{1}'.format(port, proto))
     sudo('ufw disable; echo "y" | ufw enable; sudo ufw status verbose')
