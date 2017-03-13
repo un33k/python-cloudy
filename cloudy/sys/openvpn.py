6c0bbcd5095fd4ae972496fd42bbc067052f48dc
Val Neekman <val@neekware.com>
2017-03-12 21:47:34 -0400

    add clients

--- cloudy/sys/openvpn.py
+++ cloudy/sys/openvpn.py
@@ -6,6 +6,7 @@ from fabric.api import run
 from fabric.api import task
 from fabric.api import sudo
 from fabric.api import put
+from fabric.api import get
 from fabric.api import env
 from fabric.api import settings
 from fabric.api import hide
@@ -17,7 +18,7 @@ from cloudy.sys.core import sys_mkdir
 from cloudy.util.common import sys_restart_service
 
 
-def sys_openvpn_docker_install(domain, port=1194, proto='udp', datadir="/docker/openvpn", repo='kylemanna/openvpn'):
+def sys_openvpn_docker_install(domain, port=1194, proto='udp', datadir='/docker/openvpn', repo='kylemanna/openvpn'):
     docker_name = "{proto}{port}{domain}".format(domain=domain, port=port, proto=proto)
     docker_data = '{data}/{domain}'.format(data=datadir, domain=domain)
 
@@ -57,4 +58,18 @@ def sys_openvpn_docker_conf(domain, port=1194, proto='udp'):
     sudo('systemctl daemon-reload')
     sudo('systemctl enable docker-{name}.service'.format(name=docker_name))
     sudo('systemctl start docker-{name}.service'.format(name=docker_name))
+
+
+def sys_openvpn_docker_create_client(client_name, domain, datadir='/docker/openvpn', repo='kylemanna/openvpn'):
+    """ docker openvpn create client - Ex: (cmd:)"""
+    docker_data = '{data}/{domain}'.format(data=datadir, domain=domain)
+    cmd = "docker run --rm -v {data}:/etc/openvpn  -it {repo} easyrsa build-client-full {client} nopass"
+    run(cmd.format(data=docker_data, repo=repo, client=client_name))
+
+    cmd = "docker run --rm -v {data}:/etc/openvpn {repo} ovpn_getclient {client} > /tmp/{client}.ovpn"
+    run(cmd.format(data=docker_data, repo=repo, client=client_name))
+
+    remote_file = "/tmp/{client}.ovpn".format(client=client_name)
+    local_file = "./{client}.ovpn".format(client=client_name)
+    get(remote_file, local_file)
 
