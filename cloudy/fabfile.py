32ed56e51025fb3cf57656df184199def2aac6f8
Avid Coder <avidcoderr@gmail.com>
2025-06-06 21:39:08 -0400

    req-update

--- cloudy/fabfile.py
+++
@@ -1,9 +0,0 @@
-from cloudy.db import *
-from cloudy.sys import *
-from cloudy.aws import *
-from cloudy.srv import *
-from cloudy.web import *
-import logging
-
-# Set global logging level to ERROR for cleaner output
-logging.basicConfig(level=logging.ERROR)
