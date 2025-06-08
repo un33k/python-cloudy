18c9a5fcb00111b20c179f2009dea3c31959c109
Avid Coder <avidcoderr@gmail.com>
2025-06-07 23:37:27 -0400

    psql installs well

--- fabfile.py
+++ fabfile.py
@@ -5,6 +5,7 @@ from cloudy.sys import (
     core, docker, etc, python, firewall, memcached, mount, openvpn, ports,
     postfix, python, redis, security, ssh, swap, timezone, user, vim
 )
+from cloudy.db import psql, pgis, mysql, pgpool, pgbouncer
 from cloudy.srv import (
     recipe_cache_redis,
     recipe_generic_server,
@@ -37,6 +38,12 @@ ns.add_collection(Collection.from_module(swap), name='swap')
 ns.add_collection(Collection.from_module(timezone), name='timezone')
 ns.add_collection(Collection.from_module(user), name='user')
 ns.add_collection(Collection.from_module(vim), name='vim')
+ns.add_collection(Collection.from_module(psql), name='psql')
+ns.add_collection(Collection.from_module(pgis), name='pgis')
+ns.add_collection(Collection.from_module(mysql), name='mysql')
+ns.add_collection(Collection.from_module(pgpool), name='pgpool')
+ns.add_collection(Collection.from_module(pgbouncer), name='pgbouncer')
+
 
 ns.add_collection(Collection.from_module(recipe_cache_redis), name='recipe_cache_redis')
 ns.add_collection(Collection.from_module(recipe_generic_server), name='recipe_generic_server')
