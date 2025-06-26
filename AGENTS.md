29299ca3c183d428231c99da347328241c3e1f5f
Avid Coder <avidcoderr@gmail.com>
2025-06-26 18:00:27 -0400

    clean up

---
+++ AGENTS.md
@@ -0,0 +1,1 @@
+# AGENTS.md - Ansible Cloudy Development Guide
--- tests/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Python Cloudy Test Suite
-
-This package contains tests for the Python Cloudy infrastructure automation toolkit.
-"""
